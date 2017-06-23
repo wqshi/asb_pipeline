eac30ded9aa0d31da515951586e8b84b6ea6b968
wqshi <wqshi.nudt@gmail.com>
2017-06-22 19:55:42 -0700

    Remove the debug library.

---
+++ R/s_library.R
@@ -0,0 +1,209 @@
+#This file is for the library loading;
+library(futile.logger)
+library(ROCR)
+#install.packages("ROCR")
+
+#install.packages("corrplot")
+
+library(corrplot)
+library(randomForest)
+#library(package)
+#cat("========Library===========","\n")
+library(lattice);
+cat("========Strings===========","\n")
+#install.packages("stringr") #xxx
+library(stringr)
+
+#install.packages("logging")
+library(logging)
+#install.packages("car") #xx
+library(car)
+cat("========hexbin===========","\n")
+#install.packages`("hexbin")
+library(hexbin)
+
+
+cat("========RColor===========","\n")
+library(RColorBrewer)
+
+#install.packages("VennDiagram")
+#library(VennDiagram)
+
+#options(install.packages.check.source = TRUE)
+#install.packages("gplots")
+#install.packages("caTools")
+#library(caTools)
+#library(gplots)
+
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("affyPLM")
+#install.packages("party")
+
+
+cat("========Biostrings========")
+library(Biostrings)
+library(kernlab)
+# source('E:/Projects/R/f_stripPlot.R')
+# source('E:/Projects/R/f_prepare_data.R')
+# source('E:/Projects/R/f_prepare_data2.R')
+# source('E:/Projects/R/f_lmEbTop_pSort.R')
+# #source('E:/Projects/R/f_lmEbTop_pSort2.R')
+# source('E:/Projects/R/f_assert.R')
+# source('E:/Projects/R/f_boxplot.r')
+# source("E:/Projects/R/f_sortBycol.R")
+##########limma################
+#limma
+#source("http://www.bioconductor.org/biocLite.R")
+#biocLite("limma")
+#biocLite("statmod")
+#library(limma)
+#library(preprocessCore)
+
+#########Yeast###############
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("ygs98.db")
+#biocLite("DynDoc")
+#biocLite("affyPLM")
+#library(ygs98.db)
+#library(DynDoc)
+#library(affyPLM)
+
+# preparation
+#library(GEOquery)
+#library(affy)
+#library(RColorBrewer)
+#biocLite("gcrma")
+#library(gcrma)
+#library(genefilter)
+#library(pvclust)
+
+
+#biocLite('lawstat')
+#library(lawstat)
+
+#RdBu_color <- colorRampPalette(brewer.pal(n = 10, "RdBu"));
+#cat("========loading debug==========")
+#install.packages("debug")
+#library(debug)
+
+#install.packages('rminer')
+#library(rminer)
+###
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("rtracklayer")
+#library(rtracklayer)
+
+##########SVm#########
+#install.packages("kernlab")
+
+##Go analysis
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("SNPlocs.Hsapiens.dbSNP.20120608")
+#biocLite("Biobase")
+#library(SNPlocs.Hsapiens.dbSNP.20120608)
+#library(BSgenome.Hsapiens.UCSC.hg19)
+#biocLite("biomaRt")
+#library(biomaRt)
+#library("RUnit", lib.loc="D:/Program Files/R/R-2.15.2/library")
+
+#cat('=====================loading GO.db====================')
+#library(GO.db)
+
+#cat('=====================loading org.Hs.eg.db====================')
+#library(org.Hs.eg.db)
+
+#biocLite("org.Hs.eg.db")
+#cat('=====================loading AnnotationDbi====================')
+#library(AnnotationDbi)
+
+
+
+#cat('=====================loading Biobase====================')
+#library(Biobase)
+#cat('=====================loading topGO====================')
+#library(topGO)
+
+#biocLite("ALL")
+#install.packages("Vennerable", repos="http://R-Forge.R-project.org")
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("RBGL")
+#biocLite("reshape")
+#library(RBGL)
+#library(Vennerable)
+
+#install.packages("plotrix")
+library(plotrix)
+
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("TFBSTools")
+
+
+####
+#install.packages("seqinr")
+library(seqinr)
+
+
+
+############
+cat("========RF==========")
+#install.packages("randomForest")
+library(randomForest)
+
+##############
+#install.packages("ipred")
+library(ipred)
+
+################
+#install.packages("ggplot2")
+library(ggplot2)
+
+
+################
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("rtracklayer")
+#library(rtracklayer)
+
+
+#install.packages("mlr")
+library(mlr)
+library("mlbench")
+# #install.packages("cforest",quiet = TRUE) #Not
+# install.packages("crs")
+# 
+# install.packages("pls")
+# install.packages("penalized",quiet = TRUE)
+# install.packages("rsm",quiet = TRUE)
+# 
+# install.packages("earth",quiet = TRUE)
+# #install.packages("knn",quiet = TRUE) #not
+# install.packages("gbm",quiet = TRUE)
+# install.packages("glmnet",quiet = TRUE)
+# install.packages("IBk",quiet = TRUE)
+# install.packages("kknn",quiet = TRUE)
+# #install.packages("km",quiet = TRUE) #not available
+# #install.packages("ksvm",quiet = TRUE) #not available
+# install.packages("penalized",quiet = TRUE)
+# install.packages("mars",quiet = TRUE)
+# install.packages("mob",quiet = TRUE)
+# install.packages("nnet",quiet = TRUE)
+# install.packages("pcr",quiet = TRUE)
+# install.packages("randomForest",quiet = TRUE)
+# install.packages("rpart",quiet = TRUE)
+# #install.packages("svm",quiet = TRUE) #not
+# #install.packages("rvm",quiet = TRUE) #not
+# #install.packages("blackboost",quiet = TRUE)
+#install.packages("mboost",quiet = TRUE)
+#install.packages("RWeka",quiet = TRUE)
+#install.packages("mda",quiet = TRUE)
+#install.packages("mboost",quiet = TRUE)
+#install.packages("mboost",quiet = TRUE)
+
+
+
+#Normalization method
+#install.packages("RcppArmadillo")
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("DESeq2")
+#library(DESeq2)
+
+
