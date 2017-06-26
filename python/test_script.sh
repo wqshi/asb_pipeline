6d98e51079ee6c14d0b6e61c3649f9cb4f9c596c
wqshi <wqshi.nudt@gmail.com>
2017-06-26 04:15:23 -0700

    After adding data.

--- python/test_script.sh
+++ python/test_script.sh
@@ -10,20 +10,25 @@ mode=test
 
 #Prepare the data
 
+mkdir $vcf_dir
+mkdir $fastq_dir
+
+ln -s $project_dir/data/ $project_dir/python/data/
+ln -s $project_dir/data/ $project_dir/R/data/
 
 cd $vcf_dir
 #The vcf directory(./data/raw_data/vcf/) should include: 
 #1. hg19.fa. #The reference genome gh19.
 #2. vcf file of the cell.(e.g. gm12878)
 
-#Put the variant calling data in the ./data/raw_data/vcf/
 #hg19.fa: hg19 reference genome(https://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/human_g1k_v37.fasta.gz)
 wget http://ftp-trace.ncbi.nih.gov/1000genomes/ftp/technical/reference/human_g1k_v37.fasta.gz --no-check-certificate -O hg19.fa.gz
 gzip -d hg19.fa.gz
 
 
 #gm12878.vcf: the vcf file of the target cell. Used to retrive from (ftp://ftp2.completegenomics.com/vcf_files/Build37_2.0.0/). But the link is broken now. We provide a subset of vcf file in the github.
-
+wget https://github.com/wqshi/asb_pipeline/blob/master/data/raw_data/gm12878.chr22.vcf.gz?raw=true --no-check-certificate
+gzip gm12878.chr22.vcf.gz > gm12878.vcf
 
 cd $fastq_dir
 #The fastq directory should include:
@@ -52,13 +57,18 @@ python2.7 p_novo_mapping.py --fastq_dir $fastq_dir --fastq_file sydh-gm12878-ctc
 python2.7 p_novo_mapping.py --fastq_dir $fastq_dir --fastq_file sydh-gm12878-ctcf-Rep2.fastq.gz --mode $mode
 
 
-#Extract the signal at heterozygous sites.
+#In the test case. We use existing bam file.
 if [ '$mode'=='test' ];
 then
    echo 'Test modep'
    $bam_dir=$bam_dir/test/
+   
+   cp $project_dir/data/raw_data/sydh-gm12878-ctcf-Rep1.chr22.sorted.bam $bam_dir/sydh-gm12878-ctcf-Rep1.sorted.bam
+   cp $project_dir/data/raw_data/sydh-gm12878-ctcf-Rep2.chr22.sorted.bam $bam_dir/sydh-gm12878-ctcf-Rep1.sorted.bam
 fi
 
+
+#Extract the signal at heterozygous sites.
 python2.7 p_post_process_after_asb_mapping_dp.py $cell $tf all $bed_dir $bam_dir $wgs_dir
 
 #Call ASB events.
@@ -66,8 +76,6 @@ cd ../R
 Rscript3 ./s_call_asb_events.R --base_dir $bed_dir --cell $cell --tf $tf
 
 #The final processed data files are in the $bed_dir. Named as gm12878-gm12878-ctcf.database.
-
-
 
 
 
